79339f020b01d4e6ac90b6085a7fe75c1db860ff
AbbasNO <abbasanwar@hotmail.com>
2024-03-13 20:58:31 +0100

    Created Manual Function

--- RobotArm/RobotArm.ino
+++ RobotArm/RobotArm.ino
@@ -29,28 +29,31 @@ const int joint4PotPin =A4;
 const int joint4Pin = 10;// Must use PWM enabled pin
 int joint4Value;
 
+Servo clamp;
+const int clampPin = 11;
 
-void setup() {
-  //Robojax.com multiple servo
-  base.attach(basePin); 
-  joint1.attach(joint1Pin);   
-  joint2.attach(joint2Pin); 
-  joint3.attach(joint3Pin);   
-  joint4.attach(joint4Pin);  
+void Clamp(bool on){
+  if(on){
+    clamp.write(0);
+  }else{
+    clamp.write(173);
+  }
+  delay(5);
 }
 
-void loop() {
-  //Robojax.com multiple servo
+void Manual(){ //return array pos
+
+   //Robojax.com multiple servo
   baseValue = analogRead(basePotPin); 
   baseValue = map(baseValue, 0, 1023, 0, 180);
   base.write(baseValue);
 
   joint1Value = analogRead(joint1PotPin); 
-  joint1Value = map(joint1Value, 0, 1023, 0, 180);
+  joint1Value = map(joint1Value, 0, 1023, 80, 140);
   joint1.write(joint1Value);
 
   joint2Value = analogRead(joint2PotPin); 
-  joint2Value = map(joint2Value, 0, 1023, 0, 180);
+  joint2Value = map(joint2Value, 0, 1023, 40, 160);
   joint2.write(joint2Value);
 
   joint3Value = analogRead(joint3PotPin); 
@@ -62,5 +65,21 @@ void loop() {
   joint4.write(joint4Value);
   
   delay(5);
+
+}
+
+void setup() {
+  //Robojax.com multiple servo
+  base.attach(basePin); 
+  joint1.attach(joint1Pin);   
+  joint2.attach(joint2Pin); 
+  joint3.attach(joint3Pin);   
+  joint4.attach(joint4Pin);  
+  clamp.attach(clampPin);
+}
+
+void loop() {
+ 
+  Manual();
  
 }// loop end
